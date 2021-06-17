1d2f39d0b23ba2e8c62c7c54455bde22e4c6cd11
Thierno Souleymane Diallo <thiernosouleymane24@gmail.com>
2021-06-16 22:30:37 -0400

    update the index.js file

--- src/index.js
+++ src/index.js
@@ -5,8 +5,24 @@ import ReactDOM from "react-dom";
 
 // Create a react Component
 
+const submit = () =>{
+    return "Submit!";
+}
+
 const App = () => {
-    return <div> Hi there!</div>
+
+    const greating = "Hello world!!";
+    return (
+      <div>
+          <label className="label" for="name">
+              Enter name:
+          </label>
+          <input id="name" type="text" />
+          <button style= {{backgroundColor: "blue", color: 'white'}}> {submit()}</button>
+
+          <h3> {greating}</h3>
+      </div>
+    )
 }
 
 // Take the react component and show it on the screen
