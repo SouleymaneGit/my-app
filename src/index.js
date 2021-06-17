db881be706d5aeef0d4ea864fcb40d9a8ef55178
Thierno Souleymane Diallo <thiernosouleymane24@gmail.com>
2021-06-16 21:39:46 -0400

    starting from the scratch

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Import React and ReactDOM libraries
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import react from "react";
+import ReactDOM from "react-dom";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Create a react Component
+
+const App = () => {
+    return <div> Hi there!</div>
+}
+
+// Take the react component and show it on the screen
+
+ReactDOM.render(<App />,document.querySelector('#root'))
